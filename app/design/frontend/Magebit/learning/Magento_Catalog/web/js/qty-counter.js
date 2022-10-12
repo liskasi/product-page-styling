b98dd48ed12a36a43e6a944a3a91f44a9d33deef
Elizaveta Sirotina <elizaveta.sirotina@magebit.com>
2022-10-12 17:05:45 +0300

    Updated styling, added styling for mobile, added docblocks

--- app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
@@ -1,3 +1,18 @@
+/**
+ * This file is part of the Magento Catalog package.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento Catalog
+ * to newer versions in the future.
+ *
+ * @copyright Copyright (c) 2019 Magento, Ltd. (https://vendor.com/)
+ * @license   GNU General Public License ("GPL") v3.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 define(['ko', 'uiComponent'], function (ko, Component) {
     'use strict';
     return Component.extend({
